757f86b1734e55c14e5e5b104a4ae54ea015f635
Rizal Rokhmat <rizalrokhmat@gmail.com>
2026-06-03 07:25:05 +0700

    ignore

--- Sistem-Data-Penduduk-Desa/obj/Debug/net10.0-windows/Sistem-Data-Penduduk-Desa.AssemblyInfo.cs
+++ Sistem-Data-Penduduk-Desa/obj/Debug/net10.0-windows/Sistem-Data-Penduduk-Desa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistem-Data-Penduduk-Desa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b2b2c9048e4969be86662229e9847a3055c110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b783df284f1e841341b5f2211af3372a71ec646b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistem-Data-Penduduk-Desa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistem-Data-Penduduk-Desa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
