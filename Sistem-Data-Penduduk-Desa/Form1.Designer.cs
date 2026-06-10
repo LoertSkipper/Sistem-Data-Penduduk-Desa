a0aa7377117ac8a5994b1ba8063eab8b4dfef937
Rizal Rokhmat <rizalrokhmat@gmail.com>
2026-06-10 12:58:02 +0700

    Update UI layout and control positions
    Adjusted control locations and sizes for better alignment:
    - Updated positions of `username_txt`, `label1`, `label2`, and `password_txt`.
    - Modified `login_btn` and `keluar_btn` positions.
    - Changed `login_panel` location, size, and set `Dock` to `DockStyle.Top`.
    - Resized and repositioned `pictureBox1`.
    - Updated `FormLogin` client size and reordered `login_panel` in `Controls`.

--- Sistem-Data-Penduduk-Desa/Form1.Designer.cs
+++ Sistem-Data-Penduduk-Desa/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // username_txt
             // 
-            username_txt.Location = new Point(287, 202);
+            username_txt.Location = new Point(675, 224);
             username_txt.Name = "username_txt";
             username_txt.Size = new Size(345, 31);
             username_txt.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Verdana", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(287, 165);
+            label1.Location = new Point(675, 187);
             label1.Name = "label1";
             label1.Size = new Size(114, 25);
             label1.TabIndex = 1;
@@ -64,7 +64,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Verdana", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(287, 255);
+            label2.Location = new Point(675, 277);
             label2.Name = "label2";
             label2.Size = new Size(105, 25);
             label2.TabIndex = 3;
@@ -72,7 +72,7 @@
             // 
             // password_txt
             // 
-            password_txt.Location = new Point(287, 292);
+            password_txt.Location = new Point(675, 314);
             password_txt.Name = "password_txt";
             password_txt.Size = new Size(345, 31);
             password_txt.TabIndex = 2;
@@ -81,7 +81,7 @@
             // 
             login_btn.BackColor = SystemColors.ActiveCaption;
             login_btn.ForeColor = SystemColors.Control;
-            login_btn.Location = new Point(520, 391);
+            login_btn.Location = new Point(908, 413);
             login_btn.Name = "login_btn";
             login_btn.Size = new Size(112, 34);
             login_btn.TabIndex = 4;
@@ -93,7 +93,7 @@
             // 
             keluar_btn.BackColor = SystemColors.ActiveCaption;
             keluar_btn.ForeColor = SystemColors.Control;
-            keluar_btn.Location = new Point(379, 391);
+            keluar_btn.Location = new Point(767, 413);
             keluar_btn.Name = "keluar_btn";
             keluar_btn.Size = new Size(112, 34);
             keluar_btn.TabIndex = 5;
@@ -110,17 +110,18 @@
             login_panel.Controls.Add(username_txt);
             login_panel.Controls.Add(label2);
             login_panel.Controls.Add(label1);
-            login_panel.Location = new Point(209, 104);
+            login_panel.Dock = DockStyle.Top;
+            login_panel.Location = new Point(0, 64);
             login_panel.Name = "login_panel";
-            login_panel.Size = new Size(885, 493);
+            login_panel.Size = new Size(1258, 493);
             login_panel.TabIndex = 6;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(378, 3);
+            pictureBox1.Location = new Point(55, 121);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(150, 121);
+            pictureBox1.Size = new Size(347, 302);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 6;
             pictureBox1.TabStop = false;
@@ -148,9 +149,9 @@
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1258, 664);
+            Controls.Add(login_panel);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Controls.Add(login_panel);
             Name = "FormLogin";
             Text = "Form Login";
             Load += FormLogin_Load;
